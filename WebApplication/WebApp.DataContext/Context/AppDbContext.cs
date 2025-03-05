cf2e16f25c3fae9a315e2d8820f14ce2f790d31d
Máté Karácsony <karacsonymate9@gmail.com>
2025-03-05 09:03:19 +0100

    Context, AppDbContext hozzaadva

---
+++ WebApplication/WebApp.DataContext/Context/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApp.DataContext.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApp.DataContext.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<User> User { get; set; }
+        public DbSet<UnregisteredUser> UnregisteredUser { get; set; }
+        public DbSet<RegisteredUser> RegisteredUser { get; set; }
+        public DbSet<Administrator> Administrator { get; set; }
+        public DbSet<Ticket> Ticket { get; set; }
+        public DbSet<Seat> Seat { get; set; }
+        public DbSet<Screening> Screening { get; set; }
+        public DbSet<Movie> Movie { get; set; }
+        public DbSet<Cashier> Cashier { get; set; }
+
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+    }
+}
--- WebApplication/WebApplication/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
+++ WebApplication/WebApplication/obj/Debug/net8.0/WebApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+622ea06b60324a2b3dd2b335ccedeb1db9c70de4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3ef385e36efc8f6958e0641fe61b9f824c54205b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
